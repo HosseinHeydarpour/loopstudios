ee1f5cc72a613832d9c36a3c8fff797bdb77a3f1
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-11 20:40:04 +0330

    feat: add navigation service

---
+++ src/app/layout/navigation/navigation.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Navigation } from './navigation.component';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NavigationService {
+  navigationItems: Navigation[] = [
+    {
+      label: 'About',
+      link: '#',
+    },
+    {
+      label: 'Careers',
+      link: '#',
+    },
+    {
+      label: 'Events',
+      link: '#',
+    },
+    {
+      label: 'Products',
+      link: '#',
+    },
+    {
+      label: 'Support',
+      link: '#',
+    },
+  ];
+
+  constructor() {}
+
+  get menu() {
+    return this.navigationItems;
+  }
+}
