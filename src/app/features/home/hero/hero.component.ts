eaf64518a9e31ccb6cd1e273de8ad20f56d44102
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-11 20:57:42 +0330

    feat: pass hero title to the hero component via input signal

--- src/app/features/home/hero/hero.component.ts
+++ src/app/features/home/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { NavigationComponent } from '../../../layout/navigation/navigation.component';
 import { NavigationService } from '../../../layout/navigation/navigation.service';
 
@@ -13,4 +13,6 @@ export class HeroComponent {
   navigationService = inject(NavigationService);
 
   navigationItems = this.navigationService.menu;
+
+  heroTitle = input.required<string>();
 }
