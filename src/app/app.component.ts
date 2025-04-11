0018d464fb41cd9bc1d88da49e7c28e68f0e3e01
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-11 20:55:43 +0330

    feat: add hero component to the home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from './features/home/hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'loopstudios-website';
