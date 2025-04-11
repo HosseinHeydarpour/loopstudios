2ed3ca350e528d7d4df6357acf372668680c9f73
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-11 21:26:07 +0330

    feat: add features component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './features/home/hero/hero.component';
+import { FeaturesComponent } from './features/home/features/features.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroComponent],
+  imports: [RouterOutlet, HeroComponent, FeaturesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
