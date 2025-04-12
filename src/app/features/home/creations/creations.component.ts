ca2f8cb630bfde947f79907e94d9c41d31c1c770
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-12 20:44:27 +0330

    feat: add creation projects as JSON data

--- src/app/features/home/creations/creations.component.ts
+++ src/app/features/home/creations/creations.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { ButtonPrimaryComponent } from '../../../shared/button-primary/button-primary.component';
 
+interface Projcet {
+  title: string;
+  desktopImg: string;
+  mobileImg: string;
+}
+
 @Component({
   selector: 'app-creations',
   standalone: true,
@@ -8,4 +14,47 @@ import { ButtonPrimaryComponent } from '../../../shared/button-primary/button-pr
   templateUrl: './creations.component.html',
   styleUrl: './creations.component.scss',
 })
-export class CreationsComponent {}
+export class CreationsComponent {
+  projects: Projcet[] = [
+    {
+      title: 'Deap Earth',
+      desktopImg: 'images/desktop/image-deep-earth.jpg',
+      mobileImg: 'images/mobile/image-deep-earth.jpg',
+    },
+    {
+      title: 'Night Arcade',
+      desktopImg: 'images/desktop/image-night-arcade.jpg',
+      mobileImg: 'images/mobile/image-night-arcade.jpg',
+    },
+    {
+      title: 'Soccer TeaM VR',
+      desktopImg: 'images/desktop/image-soccer-team.jpg',
+      mobileImg: 'images/mobile/image-soccer-team.jpg',
+    },
+    {
+      title: 'The Grid',
+      desktopImg: 'images/desktop/image-grid.jpg',
+      mobileImg: 'images/mobile/image-grid.jpg',
+    },
+    {
+      title: 'From above vr',
+      desktopImg: 'images/desktop/image-from-above.jpg',
+      mobileImg: 'images/mobile/image-from-above.jpg',
+    },
+    {
+      title: 'Pocket Borealis',
+      desktopImg: 'images/desktop/image-pocket-borealis.jpg',
+      mobileImg: 'images/mobile/image-pocket-borealis.jpg',
+    },
+    {
+      title: 'The Curiosity',
+      desktopImg: 'images/desktop/image-curiosity.jpg',
+      mobileImg: 'images/mobile/image-curiosity.jpg',
+    },
+    {
+      title: 'Make It Fisheye',
+      desktopImg: 'images/desktop/image-fisheye.jpg',
+      mobileImg: 'images/mobile/image-fisheye.jpg',
+    },
+  ];
+}
