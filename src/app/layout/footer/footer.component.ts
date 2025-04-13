20d8c078e82033aa33f41684122bece60b23c710
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-13 12:25:47 +0330

    feat: load copyright year from time and date JS API

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NavigationService } from '../navigation/navigation.service';
+import { NgFor } from '@angular/common';
+import { SocialsComponent } from '../../shared/socials/socials.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [NgFor, SocialsComponent],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  navigationService = inject(NavigationService);
 
+  menuItems = this.navigationService.menu;
+
+  get year() {
+    return new Date().getFullYear();
+  }
 }
